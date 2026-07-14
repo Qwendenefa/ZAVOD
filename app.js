cd026524134800ce7e59b9aa3d118a237d9e414a
Anna <anna_jukova2001@mail.ru>
2026-07-14 09:14:12 +0300

    19

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '/frontend')));
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.get('/debug-files', (req, res) => {
     const path = require('path');
